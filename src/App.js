6766a4f722717454123a55935050ed9603d14bcf
skydragon0324 <luckymannsh@gmail.com>
2023-11-30 18:49:10 +0900

    body

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import Body from './components/Body';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Header />
+      <Body />
     </div>
   );
 }
